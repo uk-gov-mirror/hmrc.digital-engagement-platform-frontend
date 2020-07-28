0483aea3a177a6eab29ddbea1ba715d0f16927a3
digital032584 <tonymyers@hotmail.com>
2020-07-28 08:16:44 +0100

    DEP-508: Passed in the div id to try to get the GTM pushed when status changes

--- public/javascripts/gtm_dl.js
+++ public/javascripts/gtm_dl.js
@@ -1,77 +1,34 @@
 //push any data-gtag objects in the format "key:value, key:value" into global dataLayer
-(function (d, w) {
+function gtmDl(d, w, el) {
   function parseData(string) {
     var properties = string.split(', ');
     var obj = {};
-    properties.forEach(function (property) {
-      var tup = property.split(':');
-      obj[tup[0]] = tup[1]
-    });
-    return obj
-  }
-
-  function waitForEl (selector, callback) {
-    if (jQuery(selector).length) {
-      callback();
-    } else {
-      setTimeout(function () {
-        waitForEl(selector, callback);
-      }, 1000);
-    }
   }
 
   function observeStatus() {
     let elementToObserve = document.querySelector("#HMRC_Fixed_1");
+    // let elementToObserve = document.querySelector("#HMRC_Fixed_1");
+    let elementToObserve = document.querySelector(el);
 
     let observer = new MutationObserver(function() {
       setAvailability();
-    });
-
-    observer.observe(elementToObserve, {subtree: true, childList: true});
-  }
 
   function setAvailability() {
     var availability;
     var nuanceText = document.querySelector('#HMRC_Fixed_1 div span').innerHTML;
+    var nuanceText = document.querySelector(el + 'div span').innerHTML;
 
     if (nuanceText === "Advisers are available to chat.") {
       availability = 'Ready';
-    } else if (nuanceText === "All of our advisers are busy at the moment. You can remain on this page and one may become available.") {
-      availability = 'Busy';
-    } else if (nuanceText === "You are in a webchat.") {
-      availability = 'In Progress';
-    } else if (nuanceText === "Our webchat is now closed.") {
-      availability = 'Offline';
-    } else {
-      availability = 'Not Responding';
-    }
-
-    addToDataLayer(availability);
-  }
-
-  function addToDataLayer (status) {
-    w.dataLayer = w.dataLayer || [];
-    var localData = d.querySelectorAll('[data-gtag]');
-
-    var localObj = {
-      'event': 'DOMContentLoaded',
-      'Status': status,
-      'Session ID': new Date().getTime() + '.' + Math.random().toString(36).substring(5),
-      'Hit TimeStamp': new Date().toUTCString()
-    };
-    Array.prototype.forEach.call(localData, function (el, i) {
-      localObj = Object.assign(localObj, parseData(el.getAttribute('data-gtag')))
-    });
-
-    w.dataLayer.push(localObj);
-  }
 
   $(window).on("load", function () {
     waitForEl('#HMRC_Fixed_1 div span', function () {
+    waitForEl(el + 'div span', function () {
       setAvailability();
       observeStatus();
     });
   });
 })(document, window);
+};
 
-GOVUK.details.init();
+gtmDl(document, window, '#HMRC_Fixed_1');
