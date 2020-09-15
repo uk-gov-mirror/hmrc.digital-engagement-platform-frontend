ad7b82ec06605db61746e3bac6f8ded97ee3d402
digital032584 <tonymyers@hotmail.com>
2020-09-15 11:54:13 +0100

    Test if adding the userAction to the url assigns the ticket to the correct deskpro queue

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -46,7 +46,9 @@ class AppConfig @Inject()(config: Configuration,
   val reportAProblemPartialUrl: String = s"$contactHost/contact/problem_reports_ajax?service=$serviceIdentifier"
   val reportAProblemNonJSUrl: String = s"$contactHost/contact/problem_reports_nonjs?service=$serviceIdentifier"
   val betaFeedbackUrl = s"$contactHost/contact/beta-feedback?service=$serviceIdentifier"
-  val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated?service=$serviceIdentifier"
+  val betaFeedbackUnauthenticatedUrl =
+    s"$contactHost/contact/beta-feedback-unauthenticated?service=$serviceIdentifier" +
+    "&userAction=/ask-hmrc/webchat/child-benefit-nuance"
 
   val nuanceUrl: String =
     "https://hmrc-uk.digital.nuance.com/chatskins/launch/inqChatLaunch10006719.js"
