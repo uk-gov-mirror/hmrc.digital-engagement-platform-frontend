9f543c783f7c59039f867e3482bdedf2688658d7
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2020-09-28 14:36:02 +0100

    Hooking a JS runner (#128)
    * first positive run
    
    * removing unneeded stuff
    
    * refactoring
    
    * removing redundant stuff
    
    * sample test
    
    * renaming
    
    * moving to root
    
    * renaming tasks to gulpUtils
    
    * simplifying implementation
    
    * removing karma stuff
    
    * removing folder

---
+++ project/JavaScriptBuild.scala
@@ -0,0 +1,31 @@
+import com.typesafe.sbt.packager.Keys._
+import play.sbt.PlayImport.PlayKeys
+import sbt.Keys._
+import sbt._
+
+object JavaScriptBuild {
+  val configDirectory = SettingKey[File]("configDirectory")
+  val runAllTests = TaskKey[Int]("runAllTests")
+  val npmInstall = TaskKey[Int]("npm-install")
+
+  val javaScriptTestRunnerHook = Seq(
+    configDirectory := {(baseDirectory in Compile)}.value,
+
+    npmInstall := {
+      val result = Gulp.npmProcess(configDirectory.value, "install").run().exitValue()
+      if (result != 0)
+        throw new Exception("npm install failed.")
+      result
+    },
+    runAllTests := {
+      val result = Gulp.gulpProcess(configDirectory.value, "test").run().exitValue()
+      if (result != 0)
+        throw new Exception("javascript tests failed")
+      result
+    },
+
+    runAllTests := {runAllTests dependsOn npmInstall}.value,
+
+    (test in Test) := {(test in Test) dependsOn runAllTests}.value,
+  )
+}
