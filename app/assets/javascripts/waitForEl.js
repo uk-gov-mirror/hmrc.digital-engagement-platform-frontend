f76f615b9b4915ad90e724201e5e1779374a5826
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2020-10-06 15:10:20 +0100

    adding waitForEl tests

--- app/assets/javascripts/waitForEl.js
+++ app/assets/javascripts/waitForEl.js
@@ -1,9 +1,9 @@
-export function waitForEl (selector, callback) {
+export function waitForEl (selector, callback, defaultTimeout = 1000) {
     if (jQuery(selector).length) {
       callback();
     } else {
       setTimeout(function () {
         waitForEl(selector, callback);
-      }, 1000);
+      }, defaultTimeout);
     }
   }
--- test/javascripts/waitForEl.spec.js
+++ test/javascripts/waitForEl.spec.js
@@ -1,11 +1,43 @@
- import * as SUT from '../../app/assets/javascripts/waitForEl'
- import jQuery from 'jquery';
+import * as SUT from '../../app/assets/javascripts/waitForEl'
 
-describe("Parse data", function() {
-	const $ = require('jquery');
-  	it("will parse the element into an object", () => {
-		const test = SUT.waitForEl("myId",()=>true);
+describe("When waiting for an element", function() {
+	
+	beforeEach(() => {
+	    jest.useFakeTimers();
+	});
 
-		expect(test.motor).toBe("v6");
+	afterEach(() => {
+		jest.clearAllTimers();	
+	});
+	it("will execute the callback if the element exists", () => {
+		var output;
+		document.body.innerHTML = `<input type="text" id="test">`
+
+		SUT.waitForEl("#test",() => {output = "success"});
+
+		expect(output).toEqual("success");
+	});
+
+	it("will wait by default 1000ms before attempting to check the element again when it is not there", () => {
+		var output;
+
+		document.body.innerHTML = `<input type="text" id="test2">`
+
+		SUT.waitForEl("#test",() => {output = "success"});
+
+		expect(setTimeout).toHaveBeenCalledTimes(1);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+	});
+
+	it("will attempt to fetch element again once wait is over", () => {
+		document.body.innerHTML = `<input type="text" id="test2">`
+
+		SUT.waitForEl("#test",() => true);
+
+		jest.runOnlyPendingTimers();
+
+		expect(setTimeout).toHaveBeenCalledTimes(2);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
 	});
 });
+
