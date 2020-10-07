01c295791f7e5451432686969eb811166ec3fc32
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2020-10-07 11:28:19 +0100

    driving behaviour for tech difficulties

--- app/assets/javascripts/waitForEl.js
+++ app/assets/javascripts/waitForEl.js
@@ -1,9 +1,9 @@
-export function waitForEl (selector, callback, defaultTimeout = 1000) {
+export function waitForEl (selector, callback, defaultTimeout = 1000, timesCheckedForElement = 1) {
     if (jQuery(selector).length) {
       callback();
     } else {
       setTimeout(function () {
-        waitForEl(selector, callback);
+        waitForEl(selector, callback, timesCheckedForElement >= 9 ? 5000 : 1000, timesCheckedForElement + 1);
       }, defaultTimeout);
     }
   }
--- test/javascripts/waitForEl.spec.js
+++ test/javascripts/waitForEl.spec.js
@@ -1,6 +1,6 @@
 import * as SUT from '../../app/assets/javascripts/waitForEl'
 
-describe("When waiting for an element", function() {
+describe("When waiting for an element", () => {
 	
 	beforeEach(() => {
 	    jest.useFakeTimers();
@@ -18,26 +18,51 @@ describe("When waiting for an element", function() {
 		expect(output).toEqual("success");
 	});
 
-	it("will wait by default 1000ms before attempting to check the element again when it is not there", () => {
-		var output;
 
-		document.body.innerHTML = `<input type="text" id="test2">`
+	describe("If the element we are waiting for is not available", () => {
+		it("will attempt to fetch element again once wait is over", () => {
+			document.body.innerHTML = `<input type="text" id="test2">`
+	
+			SUT.waitForEl("#test",() => true);
+	
+			jest.runOnlyPendingTimers();
+	
+			expect(setTimeout).toHaveBeenCalledTimes(2);
+			expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+		});
+		it("will wait by default 1000ms before attempting to check the element again", () => {
+			var output;
+	
+			document.body.innerHTML = `<input type="text" id="test2">`
+	
+			SUT.waitForEl("#test",() => {output = "success"});
+	
+			expect(setTimeout).toHaveBeenCalledTimes(1);
+			expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+		});
 
-		SUT.waitForEl("#test",() => {output = "success"});
-
-		expect(setTimeout).toHaveBeenCalledTimes(1);
-		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
-	});
-
-	it("will attempt to fetch element again once wait is over", () => {
-		document.body.innerHTML = `<input type="text" id="test2">`
-
-		SUT.waitForEl("#test",() => true);
-
-		jest.runOnlyPendingTimers();
-
-		expect(setTimeout).toHaveBeenCalledTimes(2);
-		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+		it("after 10 attempts looking for the element, it will start looking every 5000 ms", () => {
+			document.body.innerHTML = `<input type="text" id="test2">`
+	
+			SUT.waitForEl("#test",() => true);
+	
+			checkForElement9Times();
+	
+			expect(setTimeout).toHaveBeenCalledTimes(10);
+			expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 5000);
+		});
 	});
 });
+
+function checkForElement9Times() {
+	jest.runOnlyPendingTimers();
+	jest.runOnlyPendingTimers();
+	jest.runOnlyPendingTimers();
+	jest.runOnlyPendingTimers();
+	jest.runOnlyPendingTimers();
+	jest.runOnlyPendingTimers();
+	jest.runOnlyPendingTimers();
+	jest.runOnlyPendingTimers();
+	jest.runOnlyPendingTimers();
+}
 
