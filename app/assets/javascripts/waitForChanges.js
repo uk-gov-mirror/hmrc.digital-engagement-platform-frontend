6df6b5e32f4d0f349665080e2ff4954097008a20
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2020-10-08 14:16:22 +0100

    refactoring

--- app/assets/javascripts/waitForChanges.js
+++ app/assets/javascripts/waitForChanges.js
@@ -6,36 +6,20 @@ import * as dataLayerUpdater from './updateDatalayer'
 export function waitForChanges(d, w) {
   $(window).on("load", function () {
     if (window.location.pathname.includes("payment-problems")) {
-      elementWatcher.waitForEl('#pp_self_assessment_webchat', function () {
-        dataLayerUpdater.updateDataLayer('#pp_self_assessment_webchat',w,d);
-  
-        statusObserver.observeStatus('#pp_self_assessment_webchat',w,d);
-      });
-
-      elementWatcher.waitForEl('#pp_vat_webchat', function () {
-        dataLayerUpdater.updateDataLayer('#pp_vat_webchat',w,d);
-  
-        statusObserver.observeStatus('#pp_vat_webchat',w,d);
-      });
-
-      elementWatcher.waitForEl('#pp_paye_webchat', function () {
-        dataLayerUpdater.updateDataLayer('#pp_paye_webchat',w,d);
-  
-        statusObserver.observeStatus('#pp_paye_webchat',w,d);
-      });
-
-      elementWatcher.waitForEl('#pp_corporation_tax_webchat', function () {
-        dataLayerUpdater.updateDataLayer('#pp_corporation_tax_webchat',w,d);
-  
-        statusObserver.observeStatus('#pp_corporation_tax_webchat',w,d);
-      });
-
+      waitForNuanceElement('#pp_self_assessment_webchat',w,d);
+      waitForNuanceElement('#pp_vat_webchat',w,d);
+      waitForNuanceElement('#pp_paye_webchat',w,d);
+      waitForNuanceElement('#pp_corporation_tax_webchat',w,d);
     } else {
-      elementWatcher.waitForEl('#HMRC_Fixed_1', function () {
-        dataLayerUpdater.updateDataLayer('#HMRC_Fixed_1',w,d);
-  
-        statusObserver.observeStatus('#HMRC_Fixed_1',w,d);
-      });
+      waitForNuanceElement('#HMRC_Fixed_1',w,d);
     }
   });
 };
+
+function waitForNuanceElement(el,w,d) {
+  elementWatcher.waitForEl(el, function () {
+    dataLayerUpdater.updateDataLayer(el,w,d);
+
+    statusObserver.observeStatus(el,w,d);
+  });
+}
