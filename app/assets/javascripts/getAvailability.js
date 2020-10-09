111eed007debe5a7473b15373395b9db28035a54
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2020-10-09 11:37:07 +0100

    refactoring get availabilities

--- app/assets/javascripts/getAvailability.js
+++ app/assets/javascripts/getAvailability.js
@@ -1,19 +1,36 @@
-export function getAvailability(nuanceText) {
-    var availability;
-
-    if (nuanceText === "Advisers are available to chat.") {
-      availability = 'Ready';
-    } else if (nuanceText === "All of our advisers are busy at the moment. Keep checking this page until the 'speak to an adviser' link becomes available.") {
-      availability = 'Busy';
-    } else if (nuanceText === "You are in a webchat.") {
-      availability = 'In Progress';
-    } else if (nuanceText === "Our webchat is now closed.") {
-      availability = 'Offline';
-    } else if (nuanceText === "You are in a webchat. If you cannot access it, you may have another chat window open.") {
-      availability = 'In Chat';
-    } else {
-      availability = 'Not Responding';
+export function getAvailability(nuanceAvailabilityMessage) {
+    switch(nuanceAvailabilityMessage){
+      case availabilityMessages.Ready:
+        return availabilities.Ready;
+      case availabilityMessages.Busy:
+        return availabilities.Busy;
+      case availabilityMessages.InProgress:
+        return availabilities.InProgress;
+      case availabilityMessages.Offline:
+        return availabilities.Offline;
+      case availabilityMessages.InChat:
+        return availabilities.InChat;
+      default:
+        return availabilities.NotResponding; 
     }
-
-    return availability;
   }
+
+export const availabilityMessages = Object.freeze(
+    {
+      Ready: "Advisers are available to chat.", 
+      Busy: "All of our advisers are busy at the moment. Keep checking this page until the 'speak to an adviser' link becomes available.",
+      InProgress: "You are in a webchat.",
+      Offline: "Our webchat is now closed.",
+      InChat: "You are in a webchat. If you cannot access it, you may have another chat window open."
+  })
+
+
+export const availabilities = Object.freeze(
+    {
+      Ready: "Ready", 
+      Busy: "Busy",
+      InProgress: "In Progress",
+      Offline: "Offline",
+      InChat: "In Chat",
+      NotResponding: "Not Responding"
+  })
