7a8e9221a20c6b04079ed56a4cbfd8e571b56c8c
digital032584 <tonymyers@hotmail.com>
2020-11-03 13:19:53 +0000

    DEP-1053: Added page for Nuance to develop and test the full screen chat skin

---
+++ app/controllers/NuanceFullPageCUIController.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.AppConfig
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.NuanceEncryptionService
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.NuanceFullPageCUIView
+
+import scala.concurrent.Future
+
+@Singleton
+class NuanceFullPageCUIController @Inject()(
+  appConfig: AppConfig,
+  mcc: MessagesControllerComponents,
+  nuanceFullPageCUIView: NuanceFullPageCUIView,
+  nuanceEncryptionServiceView: NuanceEncryptionService) extends FrontendController(mcc) {
+
+  implicit val config: AppConfig = appConfig
+
+  def nuanceFullPageCUI: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(nuanceFullPageCUIView()))
+  }
+}
