6438dd77710966f90833c47f3d46c230adeda047
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2020-11-03 11:39:21 +0000

    refactor

--- app/assets/javascripts/waitForEl.js
+++ app/assets/javascripts/waitForEl.js
@@ -10,8 +10,8 @@ export function waitForEl (element, callback, w, defaultTimeout = 1000, timesChe
     } else {
       setTimeout(function () {
         if (timesCheckedForElement == maxNumberOfAttempts) {
-          const message = window.location.pathname.includes("virtual-assistant") ? "The digital assistant" : "Webchat"
-          $(element).text(`${message} is experiencing technical difficulties. Please keep refreshing the page to try again.`)
+          const assistantUsed = window.location.pathname.includes("virtual-assistant") ? "The digital assistant" : "Webchat"
+          $(element).text(`${assistantUsed} is experiencing technical difficulties. Please keep refreshing the page to try again.`)
           reportEvent(w,createDataLayerElement(availabilities.NuanceUnavailable, element))
           return;
         }
